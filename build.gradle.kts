e91f1efe23be12c81cf3813ab03869e89fe31d8b
TavstalDev <tavstal@gmail.com>
2025-02-21 12:00:24 +0100

    Removed unused dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "io.github.tavstal"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -14,10 +14,6 @@ repositories {
     maven {
         url = uri("https://jitpack.io")
     }
-    maven {
-        name = "placeholderApi"
-        url = uri("https://repo.extendedclip.com/content/repositories/placeholderapi/")
-    }
 }
 
 dependencies {
@@ -25,14 +21,6 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     // PaperMC dependency
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
-    // Vault API
-    compileOnly("com.github.MilkBowl:VaultAPI:1.7") {
-        exclude(group = "org.bukkit", module = "bukkit")
-    }
-    // Placeholder API
-    compileOnly("me.clip:placeholderapi:2.11.6")
-    // SpiGUI API
-    compileOnly("com.samjakob:SpiGUI:1.3.1")
     // HTTP Client
     compileOnly("org.apache.httpcomponents:httpclient:4.5.14")
     // HikariCP / Database Connection Pool
