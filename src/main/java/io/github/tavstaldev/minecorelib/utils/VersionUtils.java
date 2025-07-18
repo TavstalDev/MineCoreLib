b679f38ea915677143ba5db12ab3a19635a4b384
tavstal <tavstal@gmail.com>
2025-07-18 16:41:55 +0200

    Add isAtLeast method to check server version compatibility

--- src/main/java/io/github/tavstaldev/minecorelib/utils/VersionUtils.java
+++ src/main/java/io/github/tavstaldev/minecorelib/utils/VersionUtils.java
@@ -50,6 +50,29 @@ public class VersionUtils {
     }
 
     /**
+     * Checks if the server version is at least the specified major, minor, and patch version.
+     * This method parses the server version string and compares it against the provided version numbers.
+     * If the patch version is not specified in the server version, it defaults to 0.
+     *
+     * @param major The major version to compare against.
+     * @param minor The minor version to compare against.
+     * @param patch The patch version to compare against.
+     * @return True if the server version is at least the specified version, false otherwise.
+     */
+    public static boolean isAtLeast(int major, int minor, int patch) {
+        String[] parts = getServerVersion().split("\\.");
+        int maj = Integer.parseInt(parts[0]);
+        int min = Integer.parseInt(parts[1]);
+        int pat;
+        if (parts.length > 2) {
+            pat = Integer.parseInt(parts[2]);
+        } else {
+            pat = 0; // Default to 0 if patch version is not specified
+        }
+        return maj > major || maj == major && min > minor || maj == major && min == minor && pat >= patch;
+    }
+
+    /**
      * Determines if the server version is considered legacy.
      *
      * @return True if the server version is legacy (not at least 1.13), false otherwise.
